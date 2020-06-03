a0e49ba04f8263d63213b2cf7a8c80b22f6c95e0
matt joseloff <msjo425@gmail.com>
2020-06-03 19:49:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,5 +6,5 @@ puts " #{state[0]} | #{state[1]} | #{state[2]} "
 puts "-----------"
 puts " #{state[3]} | #{state[4]} | #{state[5]} "
 puts "-----------"
-puts " #{state[3]} | #{state[4]} | #{state[5]} "
+puts " #{state[6]} | #{state[7]} | #{state[8]} "
 end
